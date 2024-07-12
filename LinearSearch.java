5d3cb59b0b7e1f72375d216e138356c7e2800b70
Kavindu Vidushan Karunathilaka <143307455+karuna3laka@users.noreply.github.com>
2024-07-12 20:07:56 +0530

    basicOfAlgo

---
+++ LinearSearch.java
@@ -0,0 +1,21 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 5, 7, 9, 11};
+        int target = 7;
+        int result = linearSearch(numbers, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
